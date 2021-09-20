2da768dfa5ce6cacd60f5b801d912ee14e04f1e5
GomathiKrishnan <gomathikrishnan21@gmail.com>
2021-09-20 15:46:20 -0500

    Commit for Updated questions UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { JSQuestions } from '../assets/questions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-hiroku-gk';
+  JSQuestions = JSQuestions;
+  CorrectAnswer = 0;
+  hour: any;
+  minute: any;
+  interval: any;
+  second = 0;
+  millisecond: any;
+  isReadytoStart = false;
+  isSubmitted = false;
+  questionAnswers = {};
+  constructor() {}
+  checkAnswers(event: any, data: any, ques_index: any) {
+    if (event.value == data.answer) {
+      this.questionAnswers = {
+        ...this.questionAnswers,
+        [ques_index]: 'Correct',
+      };
+    } else {
+      this.questionAnswers = { ...this.questionAnswers, [ques_index]: 'Wrong' };
+    }
+    let CorrectAnswer = Object.keys(this.questionAnswers).filter((value) => {
+      return this.questionAnswers[value] === 'Correct';
+    });
+    this.CorrectAnswer = CorrectAnswer.length;
+  }
+  start() {
+    this.interval = setInterval(() => {
+      this.timer();
+    }, 1000);
+  }
+
+  timer() {
+    this.second = this.second + 1;
+  }
+
+  onSubmit() {
+    clearInterval(this.interval);
+    this.isReadytoStart = false;
+    this.isSubmitted = true;
+    console.log('second', this.second);
+  }
+  onStart() {
+    this.isReadytoStart = true;
+    this.start();
+  }
+  onTimerChallenge() {
+    console.log('second', this.second);
+  }
 }
---
+++ src/assets/questions.ts
@@ -0,0 +1,76 @@
+export const JSQuestions = {
+  header: 'Test your JS skills',
+  Questions: [
+    {
+      question: "What is the output for the following 4 + 5 + ' 7 ' + 8 + 9 ?",
+      options: ['9789', '33', '114', '9'],
+      answer: '9789',
+    },
+    {
+      question: 'Is JavaScript a case-sensitive language?',
+      options: ['Yes', 'No'],
+      answer: 'Yes',
+    },
+    {
+      question: 'Among the following which one is Java Script framework?',
+      options: ['React', 'Java', 'SCSS', 'Bootstrap'],
+      answer: 'React',
+    },
+    {
+      question: 'What is NaN in JavaScript?',
+      options: [
+        'Not a Number',
+        'Not a Null',
+        'Not a Nested Function',
+        'Not a Node',
+      ],
+      answer: 'Not a Number',
+    },
+
+    {
+      question: 'What is null in JavaScript?',
+      options: [
+        'Intentional absence of object value',
+        'Nothing',
+        'No Value',
+        'Not Declared',
+      ],
+      answer: 'Intentional absence of object value',
+    },
+    {
+      question: 'In which version of ECMAScript arrow function is introduced?',
+      options: ['6', '5', '7', '8'],
+      answer: '6',
+    },
+    {
+      question: 'Promises are used to handle which operations?',
+      options: ['synchronous', 'asynchronous'],
+      answer: 'asynchronous',
+    },
+    {
+      question: 'WeakSet supports how many methods?',
+      options: ['5', '3', '8', '4'],
+      answer: '3',
+    },
+    {
+      question: 'What is Type Coercion in JavaScript ?',
+      options: [
+        'Conversion of values from String to Integer',
+        'Conversion of values from one data type to another.',
+        'Used for changing ECMAScript version',
+        'Used for ommiting undefined values',
+      ],
+      answer: 'Conversion of values from one data type to another.',
+    },
+    {
+      question: 'What is Hoisting in JavaScript ?',
+      options: [
+        'Moving declarations to the top of their containing scope',
+        'Moving declarations outside the containing scope',
+        'Moving declarations to the bottom of their containing scope',
+        'Removing declaration from the scope',
+      ],
+      answer: 'Moving declarations to the top of their containing scope',
+    },
+  ],
+};
