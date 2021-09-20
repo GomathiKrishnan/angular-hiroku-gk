a05bd6a439a0184cc5da7c31f7facae37aab9ab3
GomathiKrishnan <gomathikrishnan21@gmail.com>
2021-09-20 16:05:16 -0500

    Removed unwanted code in server.js

--- server.js
+++ server.js
@@ -13,32 +13,3 @@ app.get("/*", (req, res) =>
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-export function sendEmail(email, time) {
-  console.log("email", email);
-  console.log("time", time);
-  var nodemailer = require("nodemailer");
-
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "learnwithfunproduct@gmail.com",
-      pass: "LearnwithFun@21",
-    },
-  });
-
-  var mailOptions = {
-    from: "JSSkillParticapant",
-    to: "learnwithfunproduct@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
-}
